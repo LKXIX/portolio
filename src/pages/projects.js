de99e6cc52c30801b70ebfeca63638261f43b57d
Liam Karlsson <karlssonliam05@gmail.com>
2024-03-31 20:02:45 +0200

    changes removed project pics

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,11 +6,11 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import proj1 from "../../public/images/projects/Malm.jpg";
-import proj2 from "../../public/images/projects/nft-collection-website-cover-image.jpg";
+/* import proj2 from "../../public/images/projects/nft-collection-website-cover-image.jpg";
 import proj3 from "../../public/images/projects/fashion-studio-website.jpg";
 import proj4 from "../../public/images/projects/portfolio-cover-image.jpg";
 import proj5 from "../../public/images/projects/agency-website-cover-image.jpg";
-import proj6 from "../../public/images/projects/devdreaming.jpg";
+import proj6 from "../../public/images/projects/devdreaming.jpg"; */
 import TransitionEffect from "@/components/TransitionEffect";
 
 const FramerImage = motion(Image);
